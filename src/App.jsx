20a0daebab162bef7261621538c3094574a564c7
Aditya Nikhade <adchamp123@gmail.com>
2025-07-13 12:30:10 +0530

    feat: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Toaster } from 'react-hot-toast'
+import FlowbuilderPage from './pages/Flowbuilder'
+
+function App() {
+
+  return (
+    <div>
+      <FlowbuilderPage/>
+      <Toaster position="top-center"/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Flowbuilder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FlowbuilderPage() {
+  return (
+    <h1 className="text-3xl font-bold text-center p-4">
+      BiteSpeed Chatbot Flow Builder
+    </h1>
+  )
+}
